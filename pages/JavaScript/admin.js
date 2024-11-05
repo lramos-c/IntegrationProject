69599284a231db6805c34fd1e6952a4fbc5d047b
ALEJANDRA MORENO CRUZ <alemorcz@gmail.com>
2024-11-05 12:21:07 -0600

    Add admin.css and admin.js update

--- pages/JavaScript/admin.js
+++ pages/JavaScript/admin.js
@@ -1,8 +1,15 @@
 // Cargar libros desde localStorage o establecer el array inicial
 let books = JSON.parse(localStorage.getItem('books')) || [
-    { img: 'https://m.media-amazon.com/images/I/713nEf55PkL._AC_UF1000,1000_QL80_.jpg', isbn: '123456', title: 'Book 1', author: 'Author 1', publisher: 'Publisher 1', description: 'Sample book 1', price: 19.99 },
-    { img: 'https://www.planetadelibros.com/usuaris/libros/fotos/377/original/portada_el-codigo-da-vinci_dan-brown_202310231128.jpg', isbn: '234567', title: 'Book 2', author: 'Author 2', publisher: 'Publisher 2', description: 'Sample book 2', price: 25.00 },
-    { img: 'https://m.media-amazon.com/images/I/81ZqSrqKT4L._AC_UF1000,1000_QL80_.jpg', isbn: '234567', title: 'Book 3', author: 'Author 3', publisher: 'Publisher 3', description: 'Sample book 2', price: 25.00 },
+    { img: 'https://m.media-amazon.com/images/I/713nEf55PkL._AC_UF1000,1000_QL80_.jpg', isbn: '123456', title: 'Book 1', author: 'Autor 1', publisher: 'Publisher 1', description: 'Sample book 1', price: 599.99 },
+    { img: 'https://www.planetadelibros.com/usuaris/libros/fotos/377/original/portada_el-codigo-da-vinci_dan-brown_202310231128.jpg', isbn: '234567', title: 'Book 2', author: 'Autor 2', publisher: 'Publisher 2', description: 'Sample book 2', price: 289.00 },
+    { img: 'https://m.media-amazon.com/images/I/81ZqSrqKT4L._AC_UF1000,1000_QL80_.jpg', isbn: '234567', title: 'Book 3', author: 'Autor 3', publisher: 'Publisher 3', description: 'Sample book 3', price: 250.00 },
+    { img: 'https://m.media-amazon.com/images/I/91Op0BdHd0L._SL1500_.jpg', isbn: '234567', title: 'Book 3', author: 'Autor 3', publisher: 'Publisher 3', description: 'Sample book 3', price: 250.00 },
+    { img: 'https://i.pinimg.com/564x/dc/0e/c0/dc0ec0d08743f30a44a384689058f2ad.jpg', isbn: '234567', title: 'Book 3', author: 'Autor 3', publisher: 'Publisher 3', description: 'Sample book 3', price: 250.00 },
+    { img: 'https://i.pinimg.com/736x/24/6a/a1/246aa11067186cdf68e93db476cf3d93.jpg', isbn: '234567', title: 'Book 3', author: 'Autor 3', publisher: 'Publisher 3', description: 'Sample book 3', price: 250.00 },
+    { img: 'https://i.pinimg.com/564x/04/67/7b/04677b90c0f9f26193224099beccade2.jpg', isbn: '234567', title: 'Book 3', author: 'Autor 3', publisher: 'Publisher 3', description: 'Sample book 3', price: 250.00 },
+    { img: 'https://i.pinimg.com/564x/98/59/f0/9859f0cb410e11204c166e7da797bd0a.jpg', isbn: '234567', title: 'Book 3', author: 'Autor 3', publisher: 'Publisher 3', description: 'Sample book 3', price: 250.00 },
+    { img: 'https://m.media-amazon.com/images/I/81ZqSrqKT4L._AC_UF1000,1000_QL80_.jpg', isbn: '234567', title: 'Book 3', author: 'Autor 3', publisher: 'Publisher 3', description: 'Sample book 3', price: 250.00 },
+    { img: 'https://m.media-amazon.com/images/I/81ZqSrqKT4L._AC_UF1000,1000_QL80_.jpg', isbn: '234567', title: 'Book 3', author: 'Autor 3', publisher: 'Publisher 3', description: 'Sample book 3', price: 250.00 },
     
 ];
 
@@ -77,9 +84,41 @@ function clearList() {
     renderBooks();
 }
 
-// Event Listeners
+
 document.getElementById('book-form').addEventListener('submit', addBook);
 document.getElementById('clear-list').addEventListener('click', clearList);
+
+function toggleActiveBook(index) {
+    const listItems = document.querySelectorAll('#book-list li');
+    listItems.forEach((item, i) => {
+        if (i === index) {
+            item.classList.toggle('active');
+        } else {
+            item.classList.remove('active'); 
+        }
+    });
+}
+
+function renderBooks() {
+    const list = document.getElementById('book-list');
+    list.innerHTML = '';
+    books.forEach((book, index) => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <img src="${book.img}" alt="${book.title}" width="100">
+            <strong>${book.title}</strong><br>
+            ISBN: ${book.isbn}<br>
+            Autor: ${book.author}<br>
+            Editorial: ${book.publisher}<br>
+            Descripción: ${book.description}<br>
+            Precio: $${book.price.toFixed(2)}
+            <button onclick="deleteBook(${index})">Eliminar</button>
+            <button onclick="editBook(${index})">Editar</button>
+        `;
+        li.addEventListener('click', () => toggleActiveBook(index));
+        list.appendChild(li);
+    });
+}
 
 // Render inicial
 renderBooks();
