d3e2ddf30c9a50b93eaa5ff9d0dbff6911d2cad9
gabriel maldonado <curtainsopened82@gmail.com>
2024-12-02 17:21:32 -0600

    Create verificarLogin and agregarUsuarioDB and modified login, signUp html

--- pages/JavaScript/logIn.js
+++ pages/JavaScript/logIn.js
@@ -1,18 +1,23 @@
+import verificarLogin from "./verificarLogin.js";
+
 // Script para iniciar sesión
 document.getElementById("loginForm").addEventListener("submit", function(event){
 event.preventDefault();
+  verificarLogin();
 // Recupera los siguientes datos de localStorage
-const userEmail = document.getElementById("email").value;
-const userPassword = document.getElementById("password").value;
+// const userEmail = document.getElementById("email").value;
+// const userPassword = document.getElementById("password").value;
 
-let user_list = new Array();
-user_list = JSON.parse(localStorage.getItem("Usuarios Registrados"))?JSON.parse(localStorage.getItem("Usuarios Registrados")):[]
-// Validación para checar que solo sea un correo por usuario
-if(user_list.some((v)=>{
-  return v.userEmail==email && v.userPassword==password
-})){
-  alert("Acceso correcto");
-}else {
-  alert("Correo y/o contraseña incorrectos")
-  }})
+// let user_list = new Array();
+// user_list = JSON.parse(localStorage.getItem("Usuarios Registrados"))?JSON.parse(localStorage.getItem("Usuarios Registrados")):[]
+// // Validación para checar que solo sea un correo por usuario
+// if(user_list.some((v)=>{
+//   return v.userEmail==email && v.userPassword==password
+// })){
+//   alert("Acceso correcto");
+//   verificarLogin();
+// }else {
+//   alert("Correo y/o contraseña incorrectos")
+//   }
+})
 
